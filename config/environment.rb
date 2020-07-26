c77b904769748cb9d344f52009faba4ee2ebbf42
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-25 19:05:12 -0700

    adds the cli class, api class, scraper class and data class

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'json'
+
+require_relative '../lib/trail.rb'
+require_relative '../lib/trail_details.rb'
+require_relative '../lib/trail_importer.rb'
+require_relative '../lib/trail_searcher.rb'
