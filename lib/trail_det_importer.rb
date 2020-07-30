ceb88023d482cf5adcc4dd6932ba47c6003802c4
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-29 19:35:01 -0700

    cleans up scraper and api methods

--- lib/trail_det_importer.rb
+++ lib/trail_det_importer.rb
@@ -4,22 +4,23 @@ class TrailDetailImporter
 
     def self.get_trail_details(trail_url)
         doc = Nokogiri::HTML(open(trail_url))
-        details = {}
-        details[:name] = doc.css("div.row").css("h1").text.strip
-        details[:difficulty] = doc.css("div.trail-subheader span.difficulty-text").text.strip
+        details = {
+        :name => doc.css("div.row").css("h1").text.strip,
+        :difficulty => doc.css("div.trail-subheader span.difficulty-text").text.strip,
+        :length => doc.css("div.mt-2#trail-stats-bar h3")[0].text.strip,
+        :route => doc.css("div.mt-2#trail-stats-bar h3")[2].text.strip,
+        :high_elev => doc.css("div.mt-2#trail-stats-bar h3")[3].css("span.imperial").text.strip,
+        :low_elev => doc.css("div.mt-2#trail-stats-bar h3")[4].css("span.imperial").text.strip,
+        :elev_gain => doc.css("div.mt-2#trail-stats-bar h3")[5].css("span.imperial").text.strip,
+        :dogs => doc.css("div.main-content-container div.mb-1 h3.inline")[0].css("span.font-body").text.strip,
+        }
         if doc.css("div.main-content-container").css("div.mb-1")[5] != nil
             details[:description] = doc.css("div.main-content-container").css("div.mb-1")[5].text.strip
         else
             details[:description] = "Not available"
         end 
-        details[:length] = doc.css("div.mt-2#trail-stats-bar h3")[0].text.strip
-        details[:route] = doc.css("div.mt-2#trail-stats-bar h3")[2].text.strip
-        details[:high_elev] = doc.css("div.mt-2#trail-stats-bar h3")[3].css("span.imperial").text.strip
-        details[:low_elev] = doc.css("div.mt-2#trail-stats-bar h3")[4].css("span.imperial").text.strip
-        details[:elev_gain] = doc.css("div.mt-2#trail-stats-bar h3")[5].css("span.imperial").text.strip
-        details[:dogs] = doc.css("div.main-content-container div.mb-1 h3.inline")[0].css("span.font-body").text.strip
-        details
-    end 
+        details 
+    end
     
 end 
 
--- lib/trail_importer.rb
+++ lib/trail_importer.rb
@@ -5,8 +5,7 @@ class TrailImporter
     @@api = "200850712-41bb2ec1278a205fdc5c9050b10c3ad2" 
 
     def self.get_trails_by_lat_long(lat, long, dist)
-        url = "https://www.hikingproject.com/data/get-trails?
-        lat=#{lat}&lon=#{long}&maxDistance=#{dist}&key=#{@@api}"
+        url = "https://www.hikingproject.com/data/get-trails?lat=#{lat}&lon=#{long}&maxDistance=#{dist}&key=#{@@api}"
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         output = JSON.parse(response.body)
@@ -18,7 +17,6 @@ class TrailImporter
                 :length => trail["length"],
                 :url => trail["url"]
             }
-            hash
         end 
     end
     
